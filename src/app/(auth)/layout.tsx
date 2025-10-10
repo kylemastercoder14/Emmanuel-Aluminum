122ab6925b911fa9ffb4fe1a8db5bbfa31495bb7
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-10-10 08:20:59 +0800

    fixed rsponsiveness

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import Navbar from '@/components/layout/navbar'
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from "react";
+import Navbar from "@/components/layout/navbar";
+import { useUser } from "@/hooks/use-user";
+import db from "@/lib/db";
 
-const AuthLayout = ({children}: {children: React.ReactNode}) => {
+const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const { user, userId } = await useUser();
+  // Only fetch order count if userId exists
+  const orderCount = userId
+    ? await db.orders.count({
+        where: {
+          userId: userId,
+        },
+      })
+    : 0;
   return (
-	<div>
-	  <Navbar />
-	  {children}
-	</div>
-  )
-}
+    <div>
+      <Navbar orderCount={orderCount} user={user} />
+      {children}
+    </div>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
